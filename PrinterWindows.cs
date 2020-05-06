e048823e9e5e10185275fde10b8cd3ef8e7a5953
HerjunoNugroho <61776693+HerjunoNugroho@users.noreply.github.com>
2020-05-06 11:32:33 +0700

    commit 1

---
+++ PrinterWindows.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class PrinterWindows
+    {
+        public string name { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Display dimension not found");
+        }
+         public  virtual void Print()
+        {
+            Console.WriteLine("Printer not found");
+        }
+    }
+}
